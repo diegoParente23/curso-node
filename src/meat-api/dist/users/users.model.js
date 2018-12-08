f271989092459ccc712f062b886e2a8fe760a233
Diego Parente <diego.parente@outlook.com.br>
2018-12-08 11:57:34 -0200

    #6 *36 Implementado modificações com middleware (pre)

--- src/meat-api/dist/users/users.model.js
+++ src/meat-api/dist/users/users.model.js
@@ -2,6 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose = require("mongoose");
 const validator_1 = require("./validator");
+const bcrypt = require("bcrypt");
+const environment_1 = require("../common/environment");
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -32,6 +34,19 @@ const userSchema = new mongoose.Schema({
             validator: validator_1.validateCPF,
             message: '{PATH}: Invalid CPF ({VALUE})'
         }
+    }
+});
+userSchema.pre('save', function (next) {
+    const user = this;
+    if (!user.isModified('password')) {
+        next();
+    }
+    else {
+        bcrypt.hash(user.password, environment_1.environment.security.saltRounds)
+            .then(hash => {
+            user.password = hash;
+            next();
+        }).catch(next);
     }
 });
 exports.User = mongoose.model('User', userSchema);
