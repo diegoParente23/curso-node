f506d5865b4923f4e086f656d14632250a4ecc30
diego.parente@outlook.com.br <diego.parente@outlook.com.br>
2019-01-19 14:11:42 -0200

    #7 *39 Schemas e sub documentos

--- src/meat-api/dist/restuarants/restaurants.model.js
+++ src/meat-api/dist/restuarants/restaurants.model.js
@@ -1,2 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose = require("mongoose");
+const menuSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    menu: {
+        type: [],
+        required: false,
+        select: false
+    }
+});
+const restSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+});
+exports.Restaurant = mongoose.model('Restaurants', restSchema);
--- src/meat-api/restuarants/restaurants.model.ts
+++ src/meat-api/restuarants/restaurants.model.ts
@@ -1,6 +1,36 @@
 import * as mongoose from 'mongoose';
 
+export interface MenuItem extends mongoose.Document {
+    name: number;
+    price: number;
+}
+
 export interface Restaurant extends mongoose.Document {
     name: string,
     menu: MenuItem[]
 }
+
+const menuSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    menu: {
+        type: [],
+        required: false,
+        select: false
+    }
+});
+
+const restSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+export const Restaurant = mongoose.model<Restaurant>('Restaurants', restSchema);
