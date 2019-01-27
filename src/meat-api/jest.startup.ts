854756e6347d37d04ccfdfef02f3b6efd0c132b4
diego.parente@outlook.com.br <diego.parente@outlook.com.br>
2019-01-27 12:11:18 -0200

    #9 *51 Inicialização global para todas as suites

---
+++ src/meat-api/jest.startup.ts
@@ -0,0 +1,34 @@
+import * as jestCli from 'jest-cli';
+
+import { environment } from "./common/environment";
+import { Server } from "./server/server";
+import { usersRouter } from "./users/users.router";
+import { reviewsRouter } from "./reviews/reviews.router";
+import { User } from "./users/users.model";
+import { Review } from "./reviews/reviews.model";
+
+let address: string;
+let server: Server;
+const beforeAllTests = () => {
+    environment.db.url = process.env.DB_URL || 'mongodb://172.18.0.2:27017/meat-api-test-db';
+    environment.server.port = process.env.SERVER_PORT || 3001;
+    address = `http://localhost:${environment.server.port}`;
+
+    server = new Server();
+    return server.bootstrap([
+        usersRouter,
+        reviewsRouter
+    ])
+    .then(() => User.remove({}).exec())
+    .then(() => Review.remove({}).exec())
+    .catch(console.error);
+};
+
+const afterAllTests = () => {
+    return server.shutdown();
+};
+
+beforeAllTests()
+    .then(() => jestCli.run())
+    .then(() => afterAllTests())
+    .catch(console.error);
